13ce72ed0e2418f3de419e9fd5e2417309f334be
Dawid Król <dawidkrol7@gmail.com>
2022-08-23 15:22:23 +0200

    Dodaj pliki projektów.

---
+++ StudentsEvents.API/Controllers/UserController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace StudentsEvents.API.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly ILogger<UserController> _logger;
+
+        public UserController(UserManager<IdentityUser> userManager, ILogger<UserController> logger)
+        {
+            _userManager = userManager;
+            _logger = logger;
+        }
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(string username, string email, string password)
+        {
+            if (string.IsNullOrEmpty(username) || string.IsNullOrEmpty(password))
+            {
+                return BadRequest();
+            }
+            var newUser = new IdentityUser()
+            {
+                UserName = username,
+                Email = email,
+            };
+
+            var result = await _userManager.CreateAsync(newUser, password);
+
+            if (result.Succeeded)
+            {
+                _logger.LogInformation("Registered user {newUser}", newUser);
+                return Ok(result);
+            }
+
+            _logger.LogError("Cannot register user {newUser}", newUser);
+            return BadRequest(result);
+        }
+    }
+}
---
+++ StudentsEvents.API/Program.cs
@@ -0,0 +1,99 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using StudentsEvents.API.DataModels;
+using System.Text;
+
+IConfiguration configuration = new ConfigurationBuilder()
+                            .AddJsonFile("appsettings.json")
+                            .Build();
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(
+        connectionString: configuration.GetConnectionString("StudentsEventsAuthDb")
+        ));
+
+builder.Services.AddIdentity<IdentityUser, IdentityRole>(config =>
+{
+    config.Password.RequireUppercase = false;
+    config.Password.RequireLowercase = false;
+    config.Password.RequiredLength = 4;
+    config.Password.RequireDigit = false;
+    config.Password.RequireNonAlphanumeric = false;
+    config.SignIn.RequireConfirmedEmail = false;
+})
+    .AddEntityFrameworkStores<ApplicationDbContext>();
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = "JwtBearer";
+    options.DefaultChallengeScheme = "JwtBearer";
+}).AddJwtBearer("JwtBearer", JWTBeareroptions =>
+{
+    JWTBeareroptions.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["Auth:Jwt:Key"])),
+        ValidateIssuer = false,
+        ValidateAudience = false,
+        ValidateLifetime = true,
+        ClockSkew = TimeSpan.FromMinutes(1)
+    };
+});
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c => {
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "Students Events API",
+        Version = "v1"
+    });
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+    {
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Description = "JWT Authorization header using the Bearer scheme. \r\n\r\n Enter 'Bearer' [space] and then your token in the text input below.\r\n\r\nExample: \"Bearer 1safsfsdfdfd\"",
+    });
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement {
+        {
+            new OpenApiSecurityScheme {
+                Reference = new OpenApiReference {
+                    Type = ReferenceType.SecurityScheme,
+                        Id = "Bearer"
+                }
+            },
+            new string[] {}
+        }
+    });
+});
+
+var app = builder.Build();
+
+app.UseStaticFiles();
+app.UseSwagger(c =>
+{
+    c.SerializeAsV2 = true;
+});
+app.UseSwaggerUI();
+
+app.UseHttpsRedirection();
+
+//using (var serviceScope = app.Services.GetService<IServiceScopeFactory>().CreateScope())
+//{
+//    var context = serviceScope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+//    context.Database.EnsureCreated();
+//}
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
