7e4b4dcc40b71b513f2f5b5289c3ff7164ae88d8
Dawid Król <dawidkrol7@gmail.com>
2022-08-23 15:33:32 +0200

    CI CD test

--- StudentsEvents.API/Controllers/WeatherForecastController.cs
+++ StudentsEvents.API/Controllers/WeatherForecastController.cs
@@ -19,6 +19,17 @@ namespace StudentsEvents.API.Controllers
             _logger = logger;
         }
         [Authorize]
+        [HttpGet(Name = "AuthorizedGetWeatherForecast")]
+        public IEnumerable<WeatherForecast> AuthorizedGet()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
         [HttpGet(Name = "GetWeatherForecast")]
         public IEnumerable<WeatherForecast> Get()
         {
