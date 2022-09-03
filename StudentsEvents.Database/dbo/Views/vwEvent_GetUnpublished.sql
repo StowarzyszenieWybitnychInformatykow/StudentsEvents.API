64c6a745f84b35befa9b69b22f4f59557fd4251e
fgieracki <filipgieracki@gmail.com>
2022-09-03 17:20:10 +0200

    fixed typo

--- StudentsEvents.Database/dbo/Views/vwEvent_GetUnpublished.sql
+++ StudentsEvents.Database/dbo/Views/vwEvent_GetUnpublished.sql
@@ -1,6 +1,6 @@
 CREATE VIEW [dbo].[vwEvent_GetUnpublished]
-  AS SELECT TOP (100) e.Id, e.Name, e.ShortDescription, e.Thumbnail, e.Website, 
-          e.Tickets, e.Latitude, e.Longitude, e.City,
+  AS SELECT TOP (100) e.Id, e.Name, e.ShortDescription, e.Thumbnail,
+          e.Website, e.Tickets, e.Latitude, e.Longitude, e.City,
           e.Region, e.Tags, e.StartDate, e.EndDate,
           e.StudentGovernmentId, e.Published, e.Owner,
           e.LastModified
